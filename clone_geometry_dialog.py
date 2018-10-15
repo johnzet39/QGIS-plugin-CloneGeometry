4bf79d4ae6ab34a459f9f0269c26778eac8c8e23
Evgeniy Z <johnzet@yandex.ru>
2018-10-15 15:51:26 +0200

    ui to py

--- clone_geometry_dialog.py
+++ clone_geometry_dialog.py
@@ -24,18 +24,12 @@
 import os
 
 from PyQt5 import QtGui, uic, QtWidgets
-
-FORM_CLASS, _ = uic.loadUiType(os.path.join(
-    os.path.dirname(__file__), 'clone_geometry_dialog_base.ui'))
+from .clone_geometry_dialog_base import Ui_CloneGeometryDialogBase
 
 
-class CloneGeometryDialog(QtWidgets.QDialog, FORM_CLASS):
-    def __init__(self, parent=None):
+
+class CloneGeometryDialog(QtWidgets.QDialog, Ui_CloneGeometryDialogBase):
+    def __init__(self):
         """Constructor."""
-        super(CloneGeometryDialog, self).__init__(parent)
-        # Set up the user interface from Designer.
-        # After setupUI you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
+        super(CloneGeometryDialog, self).__init__()
         self.setupUi(self)
