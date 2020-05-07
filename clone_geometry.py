71b8f4416e2991c762a520796cddef84b16f824a
Evgeniy Z <johnzet@yandex.ru>
2020-05-07 13:35:49 +0200

    Set crs for target layer (for Qgis 3.12(10)). Fill colors cycle. Refactor.

--- clone_geometry.py
+++ clone_geometry.py
@@ -61,23 +61,17 @@ class AbtDialog(QDialog):
 
         self.about_ui.label_6.setPixmap(QtGui.QPixmap(':/plugins/CloneGeometry/icon.png').scaled(QSize(64, 64)))
         self.about_ui.label_6.setScaledContents(False)
-        self.about_ui.label_version.setText('2017.1027')
+        self.about_ui.label_version.setText('2020.0507')
         self.about_ui.label_4.setText('Златанов Евгений')
         self.about_ui.label.setText('CloneFeatures')
-        self.about_ui.textBrowser.setText('2015-2018. Клонирование объектов в существующий слой или новый временный слой. ')
+        self.about_ui.textBrowser.setText('2015-2020. Клонирование объектов в существующий слой или новый временный слой. ')
 
 
 class CloneGeometry:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
 
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
         # Save reference to the QGIS interface
         self.iface = iface
         # initialize plugin directory
@@ -96,9 +90,18 @@ class CloneGeometry:
             if qVersion() > '4.3.3':
                 QCoreApplication.installTranslator(self.translator)
 
-        # Create the dialog (after translation) and keep reference
-        # self.mColor = "#0055ff"
-        self.mColor = QColor(0,85,255,255) #дефолтный цвет
+        self.colorValues = [
+            "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#000000",
+            "#800000", "#008000", "#000080", "#808000", "#800080", "#008080", "#808080",
+            "#C00000", "#00C000", "#0000C0", "#C0C000", "#C000C0", "#00C0C0", "#C0C0C0",
+            "#400000", "#004000", "#000040", "#404000", "#400040", "#004040", "#404040",
+            "#200000", "#002000", "#000020", "#202000", "#200020", "#002020", "#202020",
+            "#600000", "#006000", "#000060", "#606000", "#600060", "#006060", "#606060",
+            "#A00000", "#00A000", "#0000A0", "#A0A000", "#A000A0", "#00A0A0", "#A0A0A0",
+            "#E00000", "#00E000", "#0000E0", "#E0E000", "#E000E0", "#00E0E0", "#E0E0E0"
+        ]
+        self.colorIndex = 0
+        self.mColor = QColor(0,85,255,255) #default color border
 
         self.dlg = CloneGeometryDialog()
         # self.dlg.maskColor.clicked.connect(self.color_picker)
@@ -118,26 +121,16 @@ class CloneGeometry:
         self.dlg.pushButton_about.setIcon(QIcon(':/plugins/CloneGeometry/about.png'))
         self.dlg.pushButton_about.clicked.connect(self.show_about)
 
+
     # about button
     def show_about(self):
         self.ui_about.setWindowFlags(self.ui_about.windowFlags() | Qt.WindowStaysOnTopHint | Qt.WindowMinMaxButtonsHint)
         self.ui_about.show()
-
-
         # TODO: We are going to let the user set this up in a future iteration
+
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('CloneGeometry', message)
 
@@ -161,21 +154,17 @@ class CloneGeometry:
 
         if status_tip is not None:
             action.setStatusTip(status_tip)
-
         if whats_this is not None:
             action.setWhatsThis(whats_this)
-
         if add_to_toolbar:
             self.iface.digitizeToolBar().addAction(action)
-
         if add_to_menu:
             self.iface.addPluginToVectorMenu(
                 self.menu,
                 action)
-
         self.actions.append(action)
-
         return action
+
 
     def initGui(self):
 
@@ -215,17 +204,14 @@ class CloneGeometry:
 
     def check_buttons_state(self, layer=None):
         layer = self.iface.activeLayer()
-        
         if not isinstance( layer, QgsVectorLayer):
             self.clone_action.setDisabled(True)
             return
-
         if layer is not None:
             layer.selectionChanged.connect(self.on_current_layer_selection_changed)
             self.on_current_layer_selection_changed()
 
         
-
     def on_current_layer_selection_changed(self):
 
         layer = self.iface.activeLayer()
@@ -305,8 +291,7 @@ class CloneGeometry:
         return cnt
 
     def setStyleLayer( self, layer ):
-        if self.dlg.checkBox_mask.isChecked():
-            self.mColor = self.dlg.maskColor.color()
+        if self.dlg.checkBox_mask.isChecked(): # only border
             if layer.geometryType()==QgsWkbTypes.PolygonGeometry  or layer.geometryType()==QgsWkbTypes.LineGeometry :
                 properties = {'color': self.mColor.name(), 'outline_width': '0.6'}
                 symbol_layer = QgsSimpleLineSymbolLayer.create(properties)
@@ -316,134 +301,131 @@ class CloneGeometry:
                 symbol_layer = QgsSimpleMarkerSymbolLayer.create(properties)
                 layer.renderer().symbols(QgsRenderContext())[0].changeSymbolLayer(0, symbol_layer)
         else:
+            if self.colorIndex > (len(self.colorValues) - 1):
+                self.colorIndex = 0
+            color = QColor(self.colorValues[self.colorIndex])
+            self.colorIndex += 1
             if layer.geometryType()==QgsWkbTypes.PolygonGeometry :
                 layer.renderer().symbols(QgsRenderContext())[0].symbolLayer(0).setStrokeWidth(0.4)
-                layer.renderer().symbols(QgsRenderContext())[0].symbolLayer(0).setBrushStyle(Qt.Dense4Pattern) 
+                layer.renderer().symbols(QgsRenderContext())[0].symbolLayer(0).setBrushStyle(Qt.Dense4Pattern)
+                layer.renderer().symbols(QgsRenderContext())[0].symbolLayer(0).setFillColor(color)
                 layer.setFeatureBlendMode(13)
         self.iface.layerTreeView().refreshLayerSymbology(layer.id())
+
+
+    def cloneToMemory(self):
+        curlayer = self.iface.mapCanvas().currentLayer()
+        selectedFeatCount = curlayer.selectedFeatureCount()
+        geo = QgsWkbTypes.displayString(curlayer.wkbType()) # wkbType string name of geometry
+        
+        targetLayer = QgsVectorLayer(geo, self.dlg.lineEdit_2.text(), "memory")
+        targetLayer.setCrs(curlayer.sourceCrs())
+        QgsProject.instance().addMapLayer(targetLayer, False)
+        root = QgsProject.instance().layerTreeRoot()
+        
+        self.setStyleLayer(targetLayer)
+        
+        if self.dlg.checkBoxAtrib.isChecked(): #copy attributes
+            curlayer_attribute_list = curlayer.fields().toList()
+            targetLayer_attribute_list = []
+            targetLayerpr = targetLayer.dataProvider()
+            
+            for attrib in curlayer_attribute_list:
+                if targetLayer.fields().lookupField(attrib.name())==-1:
+                    targetLayer_attribute_list.append(QgsField(attrib.name(),attrib.type()))
+            with edit(targetLayer):
+                for attr in targetLayer_attribute_list:
+                    if attr.type() == 1: # иначе игнорируется поле с типом 1 (bool)
+                        attr = QgsField(attr.name(), QVariant.String) # конвертируем bool в string
+                    res_add = targetLayer.addAttribute(attr)   
+                    if not res_add:
+                        print(u'Не создано поле {}'.format(attr.name())) 
+            targetLayer.updateFields()
+
+        # for feat in curlayer.selectedFeatures(): # not work more
+        #     targetLayer.dataProvider().addFeatures([feat]) # not work more
+
+        # ИЗ МОДУЛЯ Apend Features To layer -----------------------------------------------
+        # В старом варианте в QGIS3 при добавлении объектов с отличающимся набором аттрибутов
+        # происходила задержка с выводом сообщений в логи. Что затягивало процесс.
+        mapping = dict()
+        for target_idx in targetLayer.fields().allAttributesList():
+            target_field = targetLayer.fields().field(target_idx)
+            source_idx = curlayer.fields().indexOf(target_field.name())
+            if source_idx != -1:
+                mapping[target_idx] = source_idx
+
+        features = curlayer.selectedFeatures()
+        destType = targetLayer.geometryType()
+        destIsMulti = QgsWkbTypes.isMultiType(targetLayer.wkbType())
+        new_features = []
+
+        for current, in_feature in enumerate(features):
+            attrs = {target_idx: in_feature[source_idx] for target_idx, source_idx in mapping.items()}
+            geom = QgsGeometry()
+            if in_feature.hasGeometry() and targetLayer.isSpatial():
+                # Convert geometry to match destination layer
+                # Adapted from QGIS qgisapp.cpp, pasteFromClipboard()
+                geom = in_feature.geometry()
+                if destType != QgsWkbTypes.UnknownGeometry:
+                    newGeometry = geom.convertToType(destType, destIsMulti)
+                    if newGeometry.isNull():
+                        continue
+                    geom = newGeometry
+                # Avoid intersection if enabled in digitize settings
+                geom.avoidIntersections(QgsProject.instance().avoidIntersectionsLayers())
+
+            new_feature = QgsVectorLayerUtils().createFeature(targetLayer, geom, attrs)
+            new_features.append(new_feature)
+
+        with edit(targetLayer):
+            res = targetLayer.addFeatures(new_features)
+        # ИЗ МОДУЛЯ Apend Features To layer -----------------------------------------------end
+
+        root.insertLayer(0, targetLayer)
+        self.iface.messageBar().clearWidgets()
+        self.iface.setActiveLayer(targetLayer)
+        curlayer.selectByIds([])
+
+        if res:
+            self.iface.messageBar().pushMessage(u"Выполнено", u"Склонировано {0}/{1} объектов".format(len(new_features), selectedFeatCount), duration=5, level=0)
+
+
+    def cloneToExists(self):
+
+        curlayer = self.iface.mapCanvas().currentLayer()
+        targetName = self.dlg.comboBox.currentText()
+        targetLayer = self.getVectorLayerByName(targetName)
+        
+        self.iface.setActiveLayer(curlayer)
+        self.iface.actionCopyFeatures().trigger()
+        self.iface.setActiveLayer(targetLayer)
+
+        if targetLayer.isEditable() == False:
+            targetLayer.startEditing()
+        self.iface.actionPasteFeatures().trigger()
+        
+        if targetLayer.selectedFeatureCount() ==1:
+            if targetLayer.isEditable() == False:
+                targetLayer.startEditing()
+            selb = targetLayer.selectedFeatures()
+            self.iface.openFeatureForm(targetLayer, selb[0], False, False) 
+
+        if not targetLayer.isEditable():
+            self.iface.messageBar().pushMessage(u"Не выполнено ", u"Возможно, не достаточно прав на редактирование слоя", duration=7, level=2)
+
         
     def cloneGeometry(self):
 
-        curlayer = self.iface.mapCanvas().currentLayer()
-        
         tomemory = False
         if self.dlg.radioToMemory.isChecked():  #MEMORY LAYER
-            tomemory = True
-            geo = QgsWkbTypes.displayString(curlayer.wkbType()) # wkbType string name of geometry
-            # if curlayer.geometryType()==QgsWkbTypes.PolygonGeometry :
-            #     geo = "MultiPolygon"
-            # elif curlayer.geometryType()==QgsWkbTypes.LineGeometry :
-            #     geo = "MultiLineString"
-            # elif curlayer.geometryType()==QgsWkbTypes.PointGeometry :
-            #     geo = "Point"
-            
-            LayerB = QgsVectorLayer(geo, self.dlg.lineEdit_2.text(), "memory")
-            QgsProject.instance().addMapLayer(LayerB, False)
-            root = QgsProject.instance().layerTreeRoot()
-            
-            self.setStyleLayer(LayerB)
-            
-            if self.dlg.checkBoxAtrib.isChecked():
-                curlayer_attribute_list = curlayer.fields().toList()
-                LayerB_attribute_list = []
-                LayerBpr = LayerB.dataProvider()
-                
-                for attrib in curlayer_attribute_list:
-                    if LayerB.fields().lookupField(attrib.name())==-1:
-                        LayerB_attribute_list.append(QgsField(attrib.name(),attrib.type()))
-                with edit(LayerB):
-                    for attr in LayerB_attribute_list:
-                        if attr.type() == 1: # иначе игнорируется поле с типом 1 (bool)
-                            attr = QgsField(attr.name(), QVariant.String) # конвертируем bool в string
-                        res_add = LayerB.addAttribute(attr)   
-                        if not res_add:
-                            print(u'Не создано поле {}'.format(attr.name())) 
-
-                # LayerBpr.addAttributes(LayerB_attribute_list)
-                LayerB.updateFields()
-
-            # for feat in curlayer.selectedFeatures():
-            #     LayerB.dataProvider().addFeatures([feat])
-
-
-            # ИЗ МОДУЛЯ Apend Features To layer -----------------------------------------------
-            # В старом варианте в QGIS3 при добавлении объектов с отличающимся набором аттрибутов
-            # происходила задержка с выводом сообщений в логи. Что затягивало процесс.
-            mapping = dict()
-            for target_idx in LayerB.fields().allAttributesList():
-                target_field = LayerB.fields().field(target_idx)
-                source_idx = curlayer.fields().indexOf(target_field.name())
-                if source_idx != -1:
-                    mapping[target_idx] = source_idx
-
-            features = curlayer.selectedFeatures()
-            destType = LayerB.geometryType()
-            destIsMulti = QgsWkbTypes.isMultiType(LayerB.wkbType())
-            new_features = []
-
-
-            for current, in_feature in enumerate(features):
-
-                attrs = {target_idx: in_feature[source_idx] for target_idx, source_idx in mapping.items()}
-
-                geom = QgsGeometry()
-
-                if in_feature.hasGeometry() and LayerB.isSpatial():
-                    # Convert geometry to match destination layer
-                    # Adapted from QGIS qgisapp.cpp, pasteFromClipboard()
-                    geom = in_feature.geometry()
-                    if destType != QgsWkbTypes.UnknownGeometry:
-                        newGeometry = geom.convertToType(destType, destIsMulti)
-                        if newGeometry.isNull():
-                            continue
-                        geom = newGeometry
-                    # Avoid intersection if enabled in digitize settings
-                    geom.avoidIntersections(QgsProject.instance().avoidIntersectionsLayers())
-
-                new_feature = QgsVectorLayerUtils().createFeature(LayerB, geom, attrs)
-                new_features.append(new_feature)
-
-            with edit(LayerB):
-                res = LayerB.addFeatures(new_features)
-            # ИЗ МОДУЛЯ Apend Features To layer -----------------------------------------------end
-
-
-            root.insertLayer(0, LayerB)
-            self.iface.messageBar().clearWidgets()
-            self.iface.setActiveLayer(LayerB)
-            curlayer.selectByIds([])
-
-            if res:
-                self.iface.messageBar().pushMessage(u"Выполнено", u"Склонировано {} объектов".format(len(new_features)), duration=5, level=0)
-
-        else:                                                   #EXISTS LAYER
-
-            myName = self.dlg.comboBox.currentText()
-            LayerB = self.getVectorLayerByName(myName)
-     
-     #-----
-            self.iface.setActiveLayer(curlayer)
-            self.iface.actionCopyFeatures().trigger()
-            self.iface.setActiveLayer(LayerB)
-            
-            if LayerB.isEditable() == False:
-                LayerB.startEditing()
-            self.iface.actionPasteFeatures().trigger()
-            
-            if LayerB.selectedFeatureCount() ==1:
-                if LayerB.isEditable() == False:
-                    LayerB.startEditing()
-                selb = LayerB.selectedFeatures()
-                self.iface.openFeatureForm(LayerB, selb[0], False, False)
-            
-            if LayerB.isEditable():
-                pass
-            else:
-                self.iface.messageBar().pushMessage(u"Не выполнено ", u"Возможно, не достаточно прав на редактирование слоя", duration=7, level=2)
+            self.cloneToMemory()
+        else:                                   #EXISTS LAYER
+            self.cloneToExists()
         
+
     def run(self):
         """Run method that performs all the real work"""
-
         
         if self.iface.mapCanvas().currentLayer() == None:
             self.iface.messageBar().pushMessage(u"Не выбран активный слой", u"Выберите слой, объекты которого необходимо клонировать", duration=5, level=2)
@@ -469,13 +451,9 @@ class CloneGeometry:
                 self.dlg.lineEdit_2.setText(LayerBNamePrefix+' '+str(cntName))
             else:
                 self.dlg.lineEdit_2.setText(LayerBNamePrefix)
-            
             self.dlg.label_3.setText(str(curlayer.selectedFeatureCount()))
-            # show the dialog
-            self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
 
+            self.dlg.show()
+        result = self.dlg.exec_()
+        if result:
             pass
